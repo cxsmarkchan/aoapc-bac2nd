2bb7a8743a9862ce5181afa68e527be37a1a680b
cxsmarkchan <cxsmarkchan@163.com>
2020-10-04 15:32:34 +0800

    add template

---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <stack>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <functional>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+struct QuickRead {
+  QuickRead() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+  }
+} quickread;
+
+int main() {
+#ifdef CXS_DEBUG
+  freopen("test.in", "r", stdin);
+#endif
+
+  return 0;
+}
